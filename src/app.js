e905a50063293c69c60aba9743e20a3bcaea7d96
陈海峰 <cn.cheft@gmail.com>
2017-04-20 17:45:51 +0800

    换成空格

--- src/app.js
+++ src/app.js
@@ -7,41 +7,41 @@ require('./assets/css/app.css')
 nprogress.configure({ showSpinner: false })
 
 function asyncMount(component) {
-	coala.cp = component
-	coala.cp.on('mount', function() {
-		nprogress.done()
-	})
-	coala.cp.mount('#app')
+  coala.cp = component
+  coala.cp.on('mount', function() {
+    nprogress.done()
+  })
+  coala.cp.mount('#app')
 }
 
 function mount(name, id) {
-	nprogress.start()
-	if (coala.cp) coala.cp.unmount()
-	switch (name) {
-		case 'list':
-			require.ensure([], function (require) {
-				var component = coala.component(require('./pages/list.html'))
-				asyncMount(component)
-			})
-			break
-		case 'detail':
-			require.ensure([], function (require) {
-				var detail = require('./pages/detail.html')
+  nprogress.start()
+  if (coala.cp) coala.cp.unmount()
+  switch (name) {
+    case 'list':
+      require.ensure([], function (require) {
+        var component = coala.component(require('./pages/list.html'))
+        asyncMount(component)
+      })
+      break
+    case 'detail':
+      require.ensure([], function (require) {
+        var detail = require('./pages/detail.html')
         detail.id = id
-				asyncMount(coala.component(detail))
-			})
-			break
-	}
+        asyncMount(coala.component(detail))
+      })
+      break
+  }
 }
 
 coala.cr = coala.router({
-	routes: {
-		'/': function () {
-			mount('list')
-		},
+  routes: {
+    '/': function () {
+      mount('list')
+    },
 
-		'/:id': function (id) {
-			mount('detail', id)
-		}
-	}
+    '/:id': function (id) {
+      mount('detail', id)
+    }
+  }
 })
